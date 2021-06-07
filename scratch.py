3ddfe7c1ff7278b92bda98e32cd964820918b5b0
Ginzer <brettfeld.m@gmail.com>
2021-06-07 22:58:20 +0200

    Improved

--- scratch.py
+++ scratch.py
@@ -1,9 +1,9 @@
 class Dog:
-	def __init__(self,name, age):
+	def __init__(self, name, age):
 		self.name = name
 		self.age = age
 
-	def get_name(self, name):
+	def get_name(self):
 		return self.name
 	
 	def get_age(self):
@@ -12,7 +12,10 @@ class Dog:
 	def set_age(self, age):
 		self.age = age
 
+	
+	
+
 d1_age = 23
-d2_name = "Tom"
-d = Dog(d2_name,d1_age)
-print(d.get_age())
+d1_name = "Tom"
+d = Dog(d1_name,d1_age)
+print(d.get_age(), d.get_name())
