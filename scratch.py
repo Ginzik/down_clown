cb01e03849392d81db7ecea27a4524955a81bdb5
Ginzer <brettfeld.m@gmail.com>
2021-06-07 22:06:30 +0200

    Added the scratch in

--- scratch.py
+++ scratch.py
@@ -1,12 +1,14 @@
 class Dog:
-	def __init__(self):
-		pass
+	def __init__(self,name):
+		self.name = name
+		print(name)
 	def add_one(self,x):
 		return x + 1
 	
 	def bark(self):
 		print("bark")
 
-d = Dog()
-d.bark()
-print(d.add_one(5))
+d = Dog("Tim")
+print(d.name)
+d2 = Dog("Bill")
+print(d2.name)
